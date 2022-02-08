6c50cd7f0ad717a2268b521ad146bea8344da9c5
Parth Jasani <82431676+Jasani-Parth@users.noreply.github.com>
2022-02-08 10:51:22 +0530

    Music Player

--- How To Play Music.cpp
+++ How To Play Music.cpp
@@ -8,38 +8,7 @@ using namespace std ;
 /** Music File Is in .wav format **/
 
 int main(int argc,char *argv[])
-
 {
-	for (int i = 0 ; i < 4 ; i++ )
-	{
-		if(i == 0 )
-		{
-			std::cout << "FIRST MUSIC" ;
-
-			PlaySound("/**__FILE-PATH__**/.wav",NULL,SND_SYNC) ;
-		}
-
-		if( i == 1 )
-		{
-			std::cout << "Second music" ;
-
-			PlaySound("/**__FILE-PATH__**/.wav",NULL,SND_SYNC) ;
-		}
-
-	   if(i == 2)
-	   {
-	   	    std::cout << "Third Song " ;
-
-	   	    PlaySound("/**__FILE-PATH__**/.wav",NULL,SND_SYNC) ;
-	   }
-
-	   if(i==3)
-	   {
-	   	  std::cout << "Fourth song " ; 
-
-	   	  PlaySound("/**__FILE-PATH__**/.wav",NULL,SND_SYNC) ;
-	   }
-
-	}
+	PlaySound("/**__FILE-PATH__**/.wav",NULL,SND_SYNC) ;
 	return 0 ;
 }
